1d52247bc462b572373ce0776bdd40e6a5d3850d
Guillermo Alfaro <galfarotolon@gmail.com>
2021-01-17 17:09:57 -0500

    adding movie details page with relevant info

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -21,10 +21,45 @@ const MovieDetails = () => {
             .catch(err => { console.log(err) })
     }, [])
     return (
-        <div className='movie-Details-container'>
+        <div className='details-container'>
             {
-                movieDetails && (<h2>{movieDetails.Title}</h2>)
-            }
+                movieDetails && (
+                    <>
+                        <div className='details-poster'>
+                            <img src={movieDetails.Poster} />
+                            <p>{movieDetails.Year}</p>
+                            <p>{movieDetails.Runtime}</p>
+                        </div>
+                        <div className='details-info'>
+                            <h2>{movieDetails.Title}</h2>
+
+                            <h5>{movieDetails.Genre}</h5>
+                            <p>{movieDetails.Plot}</p>
+                            <p>{movieDetails.Actors}</p>
+                            <p>{movieDetails.BoxOffice}</p>
+                            <p>{movieDetails.Director}</p>
+                            <p>{movieDetails.Rated}</p>
+                            {movieDetails.Ratings.map(rating => {
+                                console.log(rating.Source);
+
+                                return (<>
+
+                                    <h3>{rating.Source}</h3>
+                                    <p>{rating.Value}</p>
+                                </>)
+
+                            })}
+
+                            <p>{movieDetails.Awards}</p>
+                            <p>{movieDetails.Writer}</p>
+                            <p>{movieDetails.Production}</p>
+
+
+
+                        </div>
+                    </>
+
+                )}
 
 
         </div>
