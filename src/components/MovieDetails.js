f6b90b23e93900226218be7ac4f6ca53be8a82df
Guillermo Alfaro <galfarotolon@gmail.com>
2021-01-17 17:39:36 -0500

    adding movie details page with relevant info

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
-import placeholder from '../images/placeholder.png'
+import imdb from '../images/imdb.svg'
 
 const MovieDetails = () => {
 
@@ -27,11 +27,24 @@ const MovieDetails = () => {
                     <>
                         <div className='details-poster'>
                             <img src={movieDetails.Poster} />
-                            <p>{movieDetails.Year}</p>
-                            <p>{movieDetails.Runtime}</p>
+                            {movieDetails.Ratings.map(rating => {
+                                console.log(rating.Source);
+
+                                return (<div className='ratings'>
+
+
+                                    <p> <strong>{rating.Source}</strong>: {rating.Value}</p>
+                                </div>)
+
+                            })}
+
                         </div>
                         <div className='details-info'>
                             <h2>{movieDetails.Title}</h2>
+
+                            <p>{movieDetails.Year}</p>
+                            <p>{movieDetails.Runtime}</p>
+
 
                             <h5>{movieDetails.Genre}</h5>
                             <p>{movieDetails.Plot}</p>
@@ -39,16 +52,7 @@ const MovieDetails = () => {
                             <p>{movieDetails.BoxOffice}</p>
                             <p>{movieDetails.Director}</p>
                             <p>{movieDetails.Rated}</p>
-                            {movieDetails.Ratings.map(rating => {
-                                console.log(rating.Source);
 
-                                return (<>
-
-                                    <h3>{rating.Source}</h3>
-                                    <p>{rating.Value}</p>
-                                </>)
-
-                            })}
 
                             <p>{movieDetails.Awards}</p>
                             <p>{movieDetails.Writer}</p>
