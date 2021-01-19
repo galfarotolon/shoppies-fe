caf6fdfad9d096ef7a090cbdb78d0cd42cf95e42
Guillermo Alfaro <galfarotolon@gmail.com>
2021-01-19 11:17:29 -0500

    fixing bugs for deployment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,7 +7,10 @@ import placeholder from '../images/placeholder.png'
 const MovieCard = ({ movie, addToSavedList, savedFromStorage }) => {
 
 
-    savedFromStorage = []
+
+
+
+    savedFromStorage ? savedFromStorage : []
 
 
     console.log(movie);
